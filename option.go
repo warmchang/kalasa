8dd80b2874379076a8fcf850525a161932659eb5
Leon Ding <ding@ibyte.me>
2022-03-19 15:43:04 +0800

    Update option.go

--- option.go
+++ option.go
@@ -93,5 +93,11 @@ func pathBackslashes(path string) string {
 // Checks whether the target path exists
 func pathExists(path string) (bool, error) {
 	_, err := os.Stat(path)
-	return err == nil || os.IsExist(err), err
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
 }
